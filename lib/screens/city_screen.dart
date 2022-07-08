692eb23f6c233d33ba004562ef992ca1184b47cb
Andrey Dubinsky <adreyka626@mail.ru>
2022-07-08 13:39:51 +0500

    The function of finding the city from the keyboard in development

---
+++ lib/screens/city_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class CityScreen extends StatefulWidget {
+  const CityScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CityScreen> createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    cursorColor: Colors.blueGrey[800],
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Enter city name',
+                      hintStyle: TextStyle(color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.black87,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide.none),
+                      icon: Icon(Icons.location_city,
+                          color: Colors.black87, size: 40.0),
+                    ),
+                    onChanged: (value) {
+                      cityName = value;
+                    },
+                  ),
+                ),
+              ),
+              TextButton(
+                child: Text(
+                  'CHECK THE WEATHER',
+                  style: TextStyle(fontSize: 20.0, color: Colors.black87),
+                ),
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/weather_forecast_screen.dart
+++ lib/screens/weather_forecast_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:weather_forecast_app/api/weather_api.dart';
 import 'package:weather_forecast_app/models/weather_forecast_daily.dart';
+import 'package:weather_forecast_app/screens/city_screen.dart';
 import 'package:weather_forecast_app/widgets/bottom_list_view.dart';
 import 'package:weather_forecast_app/widgets/city_view.dart';
 import 'package:weather_forecast_app/widgets/detail_view.dart';
@@ -17,7 +18,8 @@ class WeatherForecastScreen extends StatefulWidget {
 class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
   late Future<WeatherForecast> forecastObject;
 
-  final String _cityName = 'Tyumen';
+  String _cityName = 'Tyumen';
+  // String _cityName;
 
   @override
   void initState() {
@@ -38,7 +40,18 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
           onPressed: () {},
         ),
         actions: <Widget>[
-          IconButton(icon: const Icon(Icons.location_city), onPressed: () {}),
+          IconButton(
+              icon: const Icon(Icons.location_city),
+              onPressed: () async {
+                var tappedName = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return CityScreen();
+                }));
+                if (tappedName != null) {
+                  _cityName = tappedName;
+                  forecastObject = WeatherApi().fetchWeatherForecastWithCity(cityName: _cityName);
+                }
+              }),
         ],
       ),
       body: ListView(
